0413fc45dbabd39e989185536b8a5f2c0c8ef9c1
wanjiru121 <rosewanjiru121@gmail.com>
2019-04-07 17:13:11 +0300

    added new files

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(a,b):
+	c = a + b
+	print(c)
+def subtract(a,b):
+	c = a - b
+	print(c)
+def multiply(a,b):
+	c = a * b
+	print(c)
+def division(a,b):
+	c = a//b
+	print(c)
+def modulus(a,b):
+	c = a % b
+	print(c)
+def percentage(a,b):
+	c = (a/b) * 100
+	print(c)
+def float(a,b):
+	c = a/b
+	print(c)
+def exponent(a,b):
+	c = a**b
+	print(c)
+	 
